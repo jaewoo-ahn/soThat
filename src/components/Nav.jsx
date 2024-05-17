56a0605eddc6581068b08a66394287ef25672304
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-17 20:59:01 +0900

    apply

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaCocktail, FaHome } from "react-icons/fa";
 import { IoMdCafe } from "react-icons/io";
-import { useMediaQuery } from "../hooks/useMediaQuery";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 const Nav = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
@@ -16,24 +16,27 @@ const Nav = () => {
           </p>
         </Link>
       </div>
-      {}
-      <div className="flex text-md items-center justify-center ">
-        <div className="nav-items ">
-          <Link to="/">
-            <FaHome size={35} />
-          </Link>
+      {isAboveMediumScreens ? (
+        <div className="flex text-md items-center justify-center ">
+          <div className="nav-items ">
+            <Link to="/">
+              <FaHome size={35} />
+            </Link>
+          </div>
+          <div className="nav-items ml-3 mt-1">
+            <Link to="/cafemenu">
+              <IoMdCafe size={35} />
+            </Link>
+          </div>
+          <div className="nav-items ml-3 mb-1 mr-3">
+            <Link to="/barmenu">
+              <FaCocktail size={35} />
+            </Link>
+          </div>
         </div>
-        <div className="nav-items ml-3 mt-1">
-          <Link to="/cafemenu">
-            <IoMdCafe size={35} />
-          </Link>
-        </div>
-        <div className="nav-items ml-3 mb-1 mr-3">
-          <Link to="/barmenu">
-            <FaCocktail size={35} />
-          </Link>
-        </div>
-      </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
