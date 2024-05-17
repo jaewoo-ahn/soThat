fe8a25fb647da767882b8bf04f519f95e1b79267
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-17 20:52:41 +0900

    navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaCocktail, FaHome } from "react-icons/fa";
+import { IoMdCafe } from "react-icons/io";
 
 const Nav = () => {
   return (
-    <div className="fixed w-full flex justify-between items-center bg-white">
+    <div className="fixed w-full flex justify-between items-center bg-[rgba(0,0,0,0.5)] rounded-b-lg shadow-lg shadow-gray-500">
       <div className="p-3">
         <Link to="/">
           <p className="text-2xl text-red-900 font-bold">
@@ -12,15 +14,21 @@ const Nav = () => {
         </Link>
       </div>
 
-      <div className="flex text-md  p-4">
-        <div className="nav-items">
-          <Link to="/">Home</Link>
+      <div className="flex text-md items-center justify-center ">
+        <div className="nav-items ">
+          <Link to="/">
+            <FaHome size={35} />
+          </Link>
         </div>
-        <div className="nav-items">
-          <Link to="/cafemenu">Cafe</Link>
+        <div className="nav-items ml-3 mt-1">
+          <Link to="/cafemenu">
+            <IoMdCafe size={35} />
+          </Link>
         </div>
-        <div className="nav-items">
-          <Link to="/barmenu">Bar</Link>
+        <div className="nav-items ml-3 mb-1 mr-3">
+          <Link to="/barmenu">
+            <FaCocktail size={35} />
+          </Link>
         </div>
       </div>
     </div>
