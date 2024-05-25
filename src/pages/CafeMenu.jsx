b334f43d8aaa983e162d328d91d0cc6e864f15cb
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-25 22:16:23 +0900

    best , signiture

--- src/pages/CafeMenu.jsx
+++ src/pages/CafeMenu.jsx
@@ -10,8 +10,16 @@ const CafeMenu = () => {
         <div>
           {cafe[0].coffee.map((res, idx) => {
             return (
-              <div key={idx}>
-                <p>{res.name}</p>
+              <div key={idx} className="flex w-full justify-between">
+                <div className="flex">
+                  <p>{res.name}</p>
+                  {res.best ? <p className="text-red-900 ml-2">BEST</p> : <></>}
+                  {res.signiture ? (
+                    <p className="text-red-900 ml-2">SIGNATURE</p>
+                  ) : (
+                    <></>
+                  )}
+                </div>
                 <p>{res.price}</p>
               </div>
             );
@@ -21,6 +29,16 @@ const CafeMenu = () => {
       </div>
       <div className="w-1/3">
         <p className="w-full border-b-2 border-black">NON Coffee</p>
+        <div>
+          {cafe[0].nonecoffee.map((res, idx) => {
+            return (
+              <div key={idx} className="flex w-full justify-between">
+                <p>{res.name}</p>
+                <p>{res.price}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
