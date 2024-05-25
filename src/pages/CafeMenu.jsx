0be9593336b7452fda351127fe051c4bfdc6b0b7
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-25 22:12:14 +0900

    map menu

--- src/pages/CafeMenu.jsx
+++ src/pages/CafeMenu.jsx
@@ -7,6 +7,17 @@ const CafeMenu = () => {
     <div className="flex h-screen items-center justify-around">
       <div className="w-1/3 ">
         <p className="w-full border-b-2 border-black">Coffee</p>
+        <div>
+          {cafe[0].coffee.map((res, idx) => {
+            return (
+              <div key={idx}>
+                <p>{res.name}</p>
+                <p>{res.price}</p>
+              </div>
+            );
+          })}
+        </div>
+        <p className="">사이즈업 +2500 샷추가 +500 크림추가 +500</p>
       </div>
       <div className="w-1/3">
         <p className="w-full border-b-2 border-black">NON Coffee</p>
