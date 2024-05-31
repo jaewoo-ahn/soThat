d0c8ccb2fed611789619ce501da67e9b0442f397
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-31 20:46:58 +0900

    snack data

---
+++ src/data/snack.js
@@ -0,0 +1,11 @@
+export const snack = [
+  { name: "팝콘", price: "4,000" },
+  { name: "어니언 팝콘", price: "4,500" },
+  { name: "허니버터 팝콘", price: "4,500" },
+  { name: "치즈 팝콘", price: "4,500" },
+  { name: "칠리 팝콘", price: "4,500" },
+  { name: "아몬드 & 초콜렛", price: "3,000" },
+  { name: "나초 & 소스", price: "7,000" },
+  { name: "브리치즈 구이", price: "15,000" },
+  { name: "외부음식", price: "5,000" },
+];
