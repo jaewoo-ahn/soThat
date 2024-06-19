a130be3b6951dde637389ec71263b7c88c40a152
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-06-19 22:23:02 +0900

    fix : remove page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useLocation, Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 import { Nav } from "./components";
-import { Home, CafeMenu, BarMenu, BarSecondMenu, Snack } from "./pages";
+import { Home, CafeMenu, BarMenu, Snack } from "./pages";
 
 function App() {
   const location = useLocation();
@@ -16,7 +16,6 @@ function App() {
             <Route index element={<Home />} />
             <Route path="/cafemenu" element={<CafeMenu />} />
             <Route path="/barmenu" element={<BarMenu />} />
-            <Route path="/barsecondmenu" element={<BarSecondMenu />} />
             <Route path="/snack" element={<Snack />} />
           </Routes>
         </AnimatePresence>
--- src/pages/BarSecondMenu.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import transition from "../transition";
-
-const BarSecondMenu = () => {
-  return <div>BarSecondMenu</div>;
-};
-
-export default transition(BarSecondMenu);
